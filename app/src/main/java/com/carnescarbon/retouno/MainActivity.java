c04b23b855194f4ac37740afbbce174bca752db1
hugostevencodePoveda <HugoStevenpoveda@hotmail.com>
2021-12-06 13:30:52 -0500

    inicio de creacion de frames

--- app/src/main/java/com/carnescarbon/retouno/MainActivity.java
+++ app/src/main/java/com/carnescarbon/retouno/MainActivity.java
@@ -1,10 +1,11 @@
 package com.carnescarbon.retouno;
 
 import android.os.Bundle;
-import android.view.View;
+
+
 import android.view.Menu;
 
-import com.google.android.material.snackbar.Snackbar;
+
 import com.google.android.material.navigation.NavigationView;
 
 import androidx.navigation.NavController;
@@ -18,6 +19,7 @@ import com.carnescarbon.retouno.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    //ATRIBUTOS
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
 
@@ -29,19 +31,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarMain.toolbar);
-        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,R.id.nav_sucursal)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
