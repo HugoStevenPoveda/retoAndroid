3bd59fe840d7b5d1dfcd559c721801f23440ff58
hugostevencodePoveda <HugoStevenpoveda@hotmail.com>
2021-12-18 14:56:52 -0500

    inicio reto4

---
+++ app/src/main/java/com/carnescarbon/retouno/ui/Sucursales.java
@@ -0,0 +1,17 @@
+package com.carnescarbon.retouno.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.carnescarbon.retouno.R;
+
+public class Sucursales extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sucursales);
+
+    }
+}
